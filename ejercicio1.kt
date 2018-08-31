b010aa2cbef6f91f88b15074aac9c390ee8ea9fa
Dennis S <dselabm@hotmail.com>
2018-08-31 12:33:06 -0500

    cambio hecho por Dnnis

--- ejercicio1.kt
+++ ejercicio1.kt
@@ -1,8 +1,10 @@
-package exercise1.TallerDeProgramacionAvanzada
+﻿package exercise1.TallerDeProgramacionAvanzada
 
 fun main(args: Array<String>) {
     println("Hello, World!")
     println("Nuevo cambio")
     println("Version Development")
     println("Taller de Programación Avanzada")
+    println("Ago. 2018- Dic. 2018")
+    println(":)")
 }
